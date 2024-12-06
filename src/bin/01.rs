9a4e91c81c949b6b9266a304d13f1d19b6fa40b4
Martin Kvisvik Larsen <martin.kvisvik.larsen@hotmail.com>
2024-12-06 12:04:27 +0100

    Add initial code for day 01.

---
+++ src/bin/01.rs
@@ -0,0 +1,69 @@
+use anyhow::*;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use code_timing_macros::time_snippet;
+use const_format::concatcp;
+use regex::Regex;
+use adv_code_2024::*;
+
+const DAY: &str = "01";
+const INPUT_FILE: &str = concatcp!("input/", DAY, ".txt");
+
+const TEST: &str = "\
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
+";
+
+fn main() -> Result<()> {
+    start_day(DAY);
+
+    //region Part 1
+    println!("=== Part 1 ===");
+
+    fn part1<R: BufRead>(reader: R) -> Result<usize> {
+        // TODO: Solve Part 1 of the puzzle
+        
+        for line in reader.lines() {
+            // TODO: Regex line
+            let line = line?;
+            let re: Regex = Regex::new(r"\d+").unwrap();
+            let parts: Vec<&str> = re.split("Hello World Rust").collect();
+            println!("{parts}");
+        }
+
+        // TODO: Sort left numbers
+        // TODO: Sort right numbers
+        // TODO: Calculate differences and sum
+        
+        // println!("answer: {answer}");
+        return Ok(1);
+    }
+
+    // TODO: Set the expected answer for the test input
+    assert_eq!(0, part1(BufReader::new(TEST.as_bytes()))?);
+
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part1(input_file)?);
+    println!("Result = {}", result);
+    //endregion
+
+    //region Part 2
+    // println!("\n=== Part 2 ===");
+    //
+    // fn part2<R: BufRead>(reader: R) -> Result<usize> {
+    //     Ok(0)
+    // }
+    //
+    // assert_eq!(0, part2(BufReader::new(TEST.as_bytes()))?);
+    //
+    // let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    // let result = time_snippet!(part2(input_file)?);
+    // println!("Result = {}", result);
+    //endregion
+
+    Ok(())
+}
